ead887bafcaac3bb6ab9e142d6f600dfcee400ca
JustinHo09 <hojustin09@gmail.com>
2026-03-08 17:03:04 -0500

    I added test inputs, a method to check if its an opeerand, and started the body of the conversion method.
    I added some test input to the main method, which will print the input and result. Then I made the isOperator method to check if the character is an operant, +,-,*,/,(,), and return true if so and false if not. Then in the toPostfix method, I made the stack to store the operands encountered. Then it will itereate through the string and check what kind of character it is.

--- src/InToPost.java
+++ src/InToPost.java
@@ -1,3 +1,57 @@
+import java.util.Stack;
+
 public class InToPost {
-    
+
+    public static void main( String[] args) {
+
+        String input = "a+b*(c^d-e)^(f+g*h)-i";
+        System.out.println(input);
+        System.out.println(toPostfix(input));
+
+    }
+
+    public static String toPostfix(String s) {
+        
+        String result = "";
+        Stack <Character> operators = new Stack<>();
+        char currentChar;
+
+        for(int i = 0; i < s.length(); i++) {
+            currentChar = s.charAt(i);
+            if( !isOperator(currentChar ) ) {
+
+            }
+
+        }
+
+        return result;
+    }
+
+    private static boolean isOperator(char c) {
+
+        if ( c == '+' ) {
+            return true;
+
+        } else if ( c == '-' ){
+            return true;
+
+        } else if ( c == '*' ) {
+            return true;
+
+        } else if ( c == '/' ) {
+            return true;
+
+        } else if ( c == '^' ) {
+            return true;
+
+        } else if ( c == '(' ) {
+            return true;
+
+        } else if ( c == ')' ) {
+            return true;
+
+        }
+
+        return false;
+    }
 }
