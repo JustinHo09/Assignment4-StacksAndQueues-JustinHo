2a072f1f76091ddcdef258793e04dd7bdc5d68d3
JustinHo09 <hojustin09@gmail.com>
2026-03-08 15:10:23 -0500

    I made the main and started the isBalanced method.
    I made the main method with some test inputs set them to be printed out. I also made the header of the isBalanced method.

--- src/Balanced.java
+++ src/Balanced.java
@@ -1,5 +1,20 @@
-
+import java.util.Stack;
 
 public class Balanced {
 
+    public static void main( String[] args){
+        String input1 = "{[()]}";
+        System.out.println(input1);
+        System.out.println(isBalanced(input1));
+        String input2 = "{[(])}¿";
+        System.out.println(input2);
+        System.out.println(isBalanced(input2));
+        String input3 = "{{[[(()<>)]]}}";
+        System.out.println(input3);
+        System.out.println(isBalanced(input3));
+    }
+
+    public static String isBalanced(String s){
+        
+    }
 }
