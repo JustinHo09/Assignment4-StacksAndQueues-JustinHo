08d7fd9465b4374c99947135a831eaf3b18ae5df
JustinHo09 <hojustin09@gmail.com>
2026-03-08 16:27:49 -0500

    I made the main method and the decoder header.
    I made the main method and added some test values, which will be printed along with the result of decoder. The decoder method will take in the input string and decode it.

--- src/Decode.java
+++ src/Decode.java
@@ -1,3 +1,22 @@
 public class Decode {
-    
+
+    public static void main ( String[] args) {
+        String input1 = "3[a]2[bc]";
+        System.out.println(input1);
+        System.out.println(decoder(input1));
+        String input2 = "3[a2[c]]";
+        System.out.println(input2);
+        System.out.println(decoder(input2));
+        String input3 = "2[abc]3[cd]ef";
+        System.out.println(input3);
+        System.out.println(decoder(input3));
+    }
+
+    public static String decoder (String s) {
+        String result = "";
+
+        
+
+        return result;
+    }
 }
