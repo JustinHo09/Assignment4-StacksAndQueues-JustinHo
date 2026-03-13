e6312bce359d75846a46dccbbed1d1553338b553
JustinHo09 <hojustin09@gmail.com>
2026-03-12 19:35:33 -0500

    I added comments to balanced.
    I added comments to the balanced class. I added comments to the balanced class to make some parts of it easier to understand.

--- src/Balanced.java
+++ src/Balanced.java
@@ -1,7 +1,16 @@
 import java.util.LinkedList;
 
+/**
+ * This is the balanced calss which will call a method to determine if the input consisting of
+ * the symbols (),[],{},<>, ¿?, is a balanced pair.
+ */
 public class Balanced {
 
+    /**
+     * This is the main method which runs some test on the isBalanced method with some test strings
+     * to see if they are balanced or not. It will print the test input and the result.
+     * @param args User input but not used in this
+     */
     public static void main( String[] args){
         String input1 = "{[()]}";
         System.out.println(input1);
@@ -14,6 +23,12 @@ public class Balanced {
         System.out.println(isBalanced(input3));
     }
 
+    /**
+     * This method  will take in a string made of the proper symbols (),[],{},<>, ¿?
+     * and return yes or no depending on if its balanced or not.
+     * @param s The input string to test.
+     * @return NO if s is not balanced, and YES if s is balanced
+     */
     public static String isBalanced(String s){
 
         LinkedList <Character> symbolStack = new LinkedList<>();
@@ -35,7 +50,8 @@ public class Balanced {
                 if(symbolStack.peek() == null){
                     return "NO";
                 }
-
+                // checks if hte previous one is its matching pair since
+                // that is the only possible case since [{] would be false
                 if (isAPair(symbolStack.peek(), currentSymbol)){
                     symbolStack.remove();
                 } else {
@@ -52,6 +68,13 @@ public class Balanced {
         return "NO";
     }
 
+    /**
+     * This method will take two characters, in order, and return a boolean
+     * value to indicate if they are pairs or not.
+     * @param open The first symbol, which is usually the open one.
+     * @param close The second symbol, which is usually the closed one.
+     * @return true if the two characters are a matching pair, and false if not.
+     */
     private static boolean isAPair (char open, char close) {
 
         if( open == '{' && close == '}' ) {
